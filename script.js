eb1ec29c6d70469bf65b34542f44629208733fcf
Mikymik3 <mikeplay@free.fr>
2025-06-26 11:12:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Effet sonore au clic si besoin ou animations JS futures
+// Pour l'instant, pas de logique JS nécessaire
+
+// Exemple : animation relancee au clic sur le titre
+// document.querySelector('.title').addEventListener('click', () => {
+//   document.querySelector('.title').classList.remove('title');
+//   void document.querySelector('.title').offsetWidth;
+//   document.querySelector('.title').classList.add('title');
+// });
